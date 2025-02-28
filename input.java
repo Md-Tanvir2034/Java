a6bcdfbd006ed10ac537810983c7a776fe1d1b18
Md-Tanvir2034 <studynote2034@gmail.com>
2025-02-28 20:41:39 +0600

    learning

---
+++ input.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+
+public class input {
+    public static void main(String[] args){
+
+        Scanner sc =  new Scanner(System.in);
+        String name = sc.next();
+        
+        String fullname = sc.nextLine();
+
+        System.out.println(name);
+
+        System.out.println(fullname);
+
+    }
+
+    
+}
