24a9841cb393083141fb13dfef1d00fa4cdf42f7
Md-Tanvir2034 <studynote2034@gmail.com>
2025-02-28 20:24:23 +0600

    learning

---
+++ variable.java
@@ -0,0 +1,14 @@
+public class variable {
+    public static void main(String[] args) {
+        // variable 
+        int a = 10 ;
+        int b = 20 ;
+        String name=" tanvir" ;
+        int sum = a+b ;
+        //for output
+        System.out.println(name);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(sum);
+    }
+}
