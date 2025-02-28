d5489e69869e58e1efaf02284e13ff7f7a3a8942
Md-Tanvir2034 <studynote2034@gmail.com>
2025-02-28 17:48:52 +0600

    leaning

---
+++ main.java
@@ -0,0 +1,15 @@
+public class main {
+    public static void Main(String[] args){
+        //for output
+        System.out.print("hello world 1  " );
+        // println for new line
+        System.out.println("hello world 2  " );
+        // \n for new line
+        System.out.print("hello world 3  \n" );
+        System.out.print("hello world 4  " );
+
+
+    }
+    
+}
+ 
